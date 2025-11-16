486d049d7630fe62d78a7092f983e8a517a47cf9
blugsam <mijiyarow.leonid@gmail.com>
2025-11-16 15:17:35 +0300

    Add project files.

---
+++ src/GlfwErrorHandler.h
@@ -0,0 +1,9 @@
+#include <iostream>
+
+namespace GlfwErrorHandler
+{
+	static void GlfwErrorCallback(int error, const char* description)
+	{
+		std::cerr << "GLFW Error (" << error << "): " << description << std::endl;
+	}
+}
